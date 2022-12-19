a38fb40e0acec3785d1d5f86949ca85e803bd6cd
MVURICE <64409721+MVURICE@users.noreply.github.com>
2022-12-19 09:22:48 +0200

    first commit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script prints 3 lines but by using an array of string and a loop
+const words = ['C is fun', 'Python is cool', 'Javascript is amazing']
+
+for (let i = 0; i < 3; i++){
+  console.log(words[i]);
+}
